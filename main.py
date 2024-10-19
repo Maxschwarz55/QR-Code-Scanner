432cccbc23080be4c1f13d6cf8a8dbdd7c86a511
Maxschwarz55 <max.schwarz9@gmail.com>
2024-10-19 14:29:14 -0500

    generates random string

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import sqlite3
 import qrcode
+import random
+import string
 
-def create_qr_code():
-    pass
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choices(characters, k=length))
+    return random_string
+
+
+def create_qr_code(size):
+    qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4
+    )
+
+    # Add data to the QR code
+    qr.add_data(1)
+
+    # Compile the QR code data
+    qr.make(fit=True)
+
+    # Create an image from the QR Code instance
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Save the image
+    img.save('custom_qr.png')
+
+def main():
+    print(generate_random_string(10))
+
+main()
